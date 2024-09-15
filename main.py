4cc77e0e61348ae76233bff6f965f39a293ffe07
Vladimir <bortzvk@gmail.com>
2024-09-15 18:51:37 +0200

    changed model version

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def ask_chatgpt(messages) -> str:
     logger.info(f"Отправка сообщений в ChatGPT: {messages}")
     try:
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",  # Или "gpt-3.5-turbo"
+            model="gpt-4o-mini",  # Или "gpt-3.5-turbo"
             messages=messages
         )
         answer = response.choices[0].message['content'].strip()
