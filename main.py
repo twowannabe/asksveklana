591c00314a615881ec125275c3e765235579c898
Vladimir <bortzvk@gmail.com>
2024-12-26 17:12:19 +0100

    fixes

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from bs4 import BeautifulSoup
 from telegram.error import BadRequest, TelegramError
 
 # Вероятность случайного ответа (1 из 90)
-RANDOM_RESPONSE_CHANCE = 0.5
+RANDOM_RESPONSE_CHANCE = 0.1
 
 # Загрузка конфигурации из файла .env
 TELEGRAM_TOKEN = config('TELEGRAM_TOKEN')
